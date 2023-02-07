f12766b31c2612b711a9e87ab57dfcad0c30c975
Neto Homework <clychnik@gmail.com>
2023-02-07 08:30:31 +0300

    Tests added.

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,15 +1,18 @@
 package ru.netology.sqr;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class SQRServiceTest {
+
+    @CsvFileSource(files ="src/test/resources/SQR.scv")
     @ParameterizedTest
-    public void testInLimitsSQRService(int minValue, int maxValue) {
+    public void testInLimitsSQRService(int minValue, int maxValue, int expectedCounter) {
 
         SQRService service = new SQRService();
-//        int minValue = 200;
-//        int maxValue = 300;
         int counter = service.calculateSQR(minValue, maxValue);
-        System.out.println("Количество квадратов в диапазоне от " + minValue + " до " + maxValue + " " + counter);
+        System.out.println("Количество квадратов в диапазоне от " + minValue + " до " + maxValue + " = " + counter);
+        Assertions.assertEquals(expectedCounter, counter);
     }
 }
