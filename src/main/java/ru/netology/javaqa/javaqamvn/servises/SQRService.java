0adadcf859c140dd14ec751a515a92fba1140232
Neto Homework <clychnik@gmail.com>
2023-02-07 06:22:55 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/javaqamvn/servises/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.javaqa.javaqamvn.servises;
+
+public class SQRService {
+
+    public int calculateSQR(int minValue, int maxValue) {
+        int x = 10;
+        int counter = 0;
+        for (int i = x; i <= 99; i++) {
+            if (minValue >= (i * i);{
+        }if (maxValue <= (i * i);{
+            counter++;
+            }
+        }
+        return counter;
+    }
+}
